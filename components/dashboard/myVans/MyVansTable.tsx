b9a4abecfe1e5082ac390c242da8fea880cf7741
Mamadou Coulibaly <ctiefin@gmail.com>
2025-03-10 20:07:33 -0300

    finished with MyVans section layout and style

--- components/dashboard/myVans/MyVansTable.tsx
+++ components/dashboard/myVans/MyVansTable.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import {
+  FaMapMarkerAlt,
+  FaStar,
+  FaCheckCircle,
+  FaTimesCircle,
+  FaExclamationTriangle,
+} from "react-icons/fa";
+import { UpdateInvoice, DeleteInvoice } from "./Buttons";
 
 const Table = () => {
+  const userVans = [1, 2];
+  const available = true;
 
-    const userVans = [1, 2]
   return (
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
             {userVans?.map((van) => (
-              <div
-                key={van}
-                className="mb-2 w-full rounded-md bg-white p-4"
-              >
+              <div key={van} className="mb-2 w-full rounded-md bg-white p-4">
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
@@ -31,14 +38,11 @@ const Table = () => {
                 </div>
                 <div className="flex w-full items-center justify-between pt-4">
                   <div>
-                    <p className="text-xl font-medium">
-                      currency
-                    </p>
+                    <p className="text-xl font-medium">currency</p>
                     <p>date</p>
                   </div>
                   <div className="flex justify-end gap-2">
-                    update invoice
-                    delete invoice
+                    update invoice delete invoice
                   </div>
                 </div>
               </div>
@@ -48,22 +52,13 @@ const Table = () => {
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Customer
+                  Vans
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Email
+                  Price
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Amount
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Date
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Status
-                </th>
-                <th scope="col" className="relative py-3 pl-6 pr-3">
-                  <span className="sr-only">Edit</span>
+                  Availability
                 </th>
               </tr>
             </thead>
@@ -75,32 +70,44 @@ const Table = () => {
                 >
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
-                      {/* <Image
-                        src={invoice.image_url}
-                        className="rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`${invoice.name}'s profile picture`}
-                      /> */}
+                      <div className="relative size-30">
+                        <Image
+                          src="/van1_thumbnail.jpg"
+                          className="rounded-lg"
+                          alt={`Van's thumbnail`}
+                          layout="fill"
+                        />
+                      </div>
+
                       <p>Van Name</p>
                     </div>
                   </td>
+                  <td className="whitespace-nowrap px-3 py-3 ">$200</td>
                   <td className="whitespace-nowrap px-3 py-3">
-                    Email
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    Format Currency
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    format Date
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    Invoice Status
+                    <span
+                      className={`flex justify-center items-center rounded-lg px-3 py-1 text-sm font-semibold ${
+                        available
+                          ? "bg-green-100 text-green-700"
+                          : "bg-red-100 text-red-700"
+                      }`}
+                    >
+                      {available ? (
+                        <>
+                          <FaCheckCircle className="mr-1 text-green-600" />{" "}
+                          Available
+                        </>
+                      ) : (
+                        <>
+                          <FaExclamationTriangle className="mr-1 text-red-600" />{" "}
+                          Not Available
+                        </>
+                      )}
+                    </span>
                   </td>
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex justify-end gap-3">
-                      update invoice
-                      delete invoice
+                    <UpdateInvoice id="id" />
+                    <DeleteInvoice id="id" />
                     </div>
                   </td>
                 </tr>
@@ -110,7 +117,10 @@ const Table = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
+
+// TODO: Define the style of the table as it should look normally for each sections
+//  TODO: Edit the database so that i can use the info to display
