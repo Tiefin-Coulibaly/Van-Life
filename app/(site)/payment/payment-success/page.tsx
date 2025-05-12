926cacdc5ad5523224c40537f2bc103c4a5096c7
Mamadou Coulibaly <ctiefin@gmail.com>
2025-05-12 00:07:09 -0300

    removed metadata from the payment success page

--- app/(site)/payment/payment-success/page.tsx
+++ app/(site)/payment/payment-success/page.tsx
@@ -1,16 +1,16 @@
 "use client";
-import { auth } from "@/auth";
+// import { auth } from "@/auth";
 import PaymentSuccessPage from "@/components/payment/PaymentSuccess";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { use } from 'react';
 import { useSession } from "next-auth/react"
 
-export const metadata: Metadata = {
-  title: "Payment Successful | Van Life",
-  description:
-    "Your van booking has been confirmed. View your booking details and receipt for your Van Life adventure.",
-};
+// export const metadata: Metadata = {
+//   title: "Payment Successful | Van Life",
+//   description:
+//     "Your van booking has been confirmed. View your booking details and receipt for your Van Life adventure.",
+// };
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
